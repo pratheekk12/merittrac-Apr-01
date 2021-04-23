5fbc6fcb5be4c67bd7c117f629dbbfe160f46d6a
root <grssl@grassrootsbpo.com>
2021-04-23 07:31:26 +0000

    latest code 23 04

---
+++ build1/precache-manifest.b90fe5f2ae41db1bb313fcf16f49ca15.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65efcdf05de39132bcdae4339e5bb64b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b82488243039612d184",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "1b82488243039612d184",
+    "url": "/static/js/2.ab585af4.chunk.js"
+  },
+  {
+    "revision": "00b7d0402c0b5220402faef5460d19c7",
+    "url": "/static/js/2.ab585af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a04bd731f01a5ea6ed5",
+    "url": "/static/js/3.b54b3b43.chunk.js"
+  },
+  {
+    "revision": "fe36e416dafbfc1c321a",
+    "url": "/static/js/4.523a24c6.chunk.js"
+  },
+  {
+    "revision": "9e8bcbe37a190c4c2570",
+    "url": "/static/js/5.63845e5d.chunk.js"
+  },
+  {
+    "revision": "843c57113209749c3826",
+    "url": "/static/js/6.2dc25d85.chunk.js"
+  },
+  {
+    "revision": "c5a26182ca5b63fef9a6",
+    "url": "/static/js/7.cc227dd6.chunk.js"
+  },
+  {
+    "revision": "3d81e0536d323e9c316f",
+    "url": "/static/js/8.89aa509b.chunk.js"
+  },
+  {
+    "revision": "56bd93be092130db9d4d",
+    "url": "/static/js/main.d814d0d3.chunk.js"
+  },
+  {
+    "revision": "c2598b8bdb0612d9e447",
+    "url": "/static/js/runtime-main.73571f15.js"
+  }
+]);
